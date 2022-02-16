a253214d36192cadcfe800d72d2f901c6f996a1c
Jonas Ermert <ermert.jonas@yahoo.de>
2022-02-16 11:03:47 +0100

    CovidTracker Java Application implemented, tested and finished

---
+++ src/main/java/com/jonasermert/covidtracker/controller/HomeController.java
@@ -0,0 +1,37 @@
+package com.jonasermert.covidtracker.controller;
+
+
+import com.jonasermert.covidtracker.models.LocationStats;
+import com.jonasermert.covidtracker.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model){
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalReportedCases =allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        model.addAttribute("locationStats",allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+
+        return "home";
+
+
+    }
+
+
+
+
+
+}
